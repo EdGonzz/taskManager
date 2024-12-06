bf6814d73f1f02e7bc35983d79a9f3e4a3162ef0
Edwin Contreras <ed25gonz@gmail.com>
2024-12-05 22:53:54 -0400

    refactor task list UI and functionality; add task editing, deletion, and checking features; update styling for better visuals

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,22 @@ $toggleThemeBtn.addEventListener('click', () => {
 function addTask(value) {
   if (value !== null && value !== '' && value !== undefined) {
     const $taskList = document.createElement('li');
-    $taskList.innerText = value;
     $taskList.classList.add('task-list');
+
+    const $taskText = document.createElement('span');
+    $taskText.innerText = value;
+    $taskText.setAttribute('id', 'task-text');
+    $taskText.classList.add('task-text');
+
+    $taskList.appendChild($taskText);
 
     $listContainer.appendChild($taskList);
 
-    createActionsBtn($taskList);
+    createActionsBtn($taskList, $taskText);
   }
 }
 
-function createActionsBtn(reference) {
+function createActionsBtn(parentList, childList) {
   const $check = document.createElement('span');
   $check.classList.add('check');
   $check.setAttribute('id', 'check');
@@ -28,8 +34,13 @@ function createActionsBtn(reference) {
   $delete.classList.add('delete');
   $delete.setAttribute('id', 'delete');
 
-  reference.insertAdjacentElement('afterbegin', $check);
-  reference.insertAdjacentElement('beforeend', $delete);
+  const $edit = document.createElement('span');
+  $edit.classList.add('pencil');
+  $edit.setAttribute('id', 'edit');
+
+  parentList.insertAdjacentElement('afterbegin', $check);
+  parentList.insertAdjacentElement('beforeend', $delete);
+  childList.insertAdjacentElement('beforeend', $edit);
 }
 
 $addTaskForm.addEventListener('submit', (e) => {
@@ -37,4 +48,35 @@ $addTaskForm.addEventListener('submit', (e) => {
   const $taskInput = document.getElementById('task-input');
 
   addTask($taskInput.value);
+});
+
+function editTask(element) {
+  if (element.parentElement.classList.contains('checked')) {
+    element.setAttribute('disabled', true);
+  } else {
+    let replace = prompt('Enter the new task name:', element.firstChild.textContent);
+    if (replace !== null && replace !== '' && replace !== undefined) {
+      element.firstChild.textContent = replace;
+    }
+  }
+}
+
+function deleteTask(element) {
+  if (confirm('Are you sure you want to delete this task?')) {
+    element.remove();
+  }
+}
+
+function checkTask(element) {
+  element.classList.toggle('checked');
+}
+
+$listContainer.addEventListener('click', (e) => {
+  if (e.target.id === 'delete') {
+    deleteTask(e.target.parentElement);
+  } else if (e.target.id === 'edit') {
+    editTask(e.target.parentElement);
+  } else if (e.target.id === 'check') {
+    checkTask(e.target.parentElement);
+  }
 });
